d3048351bca1b65dc7221ff7ffc5cca239068b35
B6137151 <59865985+B6137151@users.noreply.github.com>
2023-03-30 04:36:57 +0700

    hhhhh

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	// Replace username, password, and dbname with your MySQL credentials
-	db, err := sql.Open("mysql", "username:password@/dbname")
+	db, err := sql.Open("mysql", "library_app_user:4x@mpL3$pA$$w0rd@/library_resources")
 	if err != nil {
 		panic(err)
 	}
@@ -27,6 +27,13 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	// Resource-related endpoints
+	r.POST("/resources", createResource)
+	r.GET("/resources/:id", getResource)
+	r.GET("/resources", getResources)
+	r.PUT("/resources/:id", updateResource)
+	r.DELETE("/resources/:id", deleteResource)
 
 	r.Run() // Listen and serve on 0.0.0.0:8080
 }
